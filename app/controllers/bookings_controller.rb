c79ae26d205f4626fed6a35833780004bc33e094
gyorgyi farkas <gyfarkas24@gmail.com>
2021-05-27 17:19:03 +0200

    booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,19 +1,40 @@
 class BookingsController < ApplicationController
-   def index
-    @bookings = Booking.where(user:current_user)
+
+  def new
+    @booking = Booking.new
+    @field = Field.find(params[:field_id])
+    authorize @field
   end
 
   def create
     @booking = Booking.new(set_booking_params)
-    offer = Offer.find(params[:offer_id])
-    @booking.offer = offer
+    @field = Field.find(params[:field_id])
+    @booking.field = @field
     @booking.user = current_user
-    @booking.status = "pending"
+    # @booking.status = "pending"
+    authorize @booking
+    authorize @field
     if @booking.save
-      redirect_to bookings_path
+      redirect_to field_path(@field)
     else
-      redirect_to offer_path(offer)
+      redirect_to field_path(@field)
     end
+  end
+
+
+
+
+    # def destroy
+    # @booking = Booking.find(params[:id])
+    # @booking.destroy
+    # redirect_to bookings_path
+    # end
+
+
+
+  private
+  def set_booking_params
+    params.require(:booking).permit(:starting_date, :ending_date)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :fields, only: [:index, :show, :new, :create] do
-    resources :bookings, only: [:create]
+    resources :bookings, only: [:create, :new]
   end
-  resources :bookings, only: [:index, :new, :destroy]
+  resources :bookings, only: [:index,:destroy]
 end
