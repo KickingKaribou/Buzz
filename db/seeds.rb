750069aed116f6193a551f9792148b85b07f7f38
Lisa Keßler <lisa-kessler@online.de>
2021-05-28 15:43:25 +0200

    index_html

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Field.destroy_all
+User.destroy_all
 puts 'Creating users...'
 tester = User.new(user_name: "Tes", email: "test@test.com", password: "123456")
 tester.save!
